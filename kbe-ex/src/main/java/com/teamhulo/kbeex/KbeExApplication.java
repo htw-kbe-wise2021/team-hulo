7803eef47f3955e5f31d4dcb5ddb1b73bbf09f70
hunsri <richard.hunstock@gmx.de>
2021-10-30 18:31:16 +0200

    added maven

---
+++ kbe-ex/src/main/java/com/teamhulo/kbeex/KbeExApplication.java
@@ -0,0 +1,13 @@
+package com.teamhulo.kbeex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeExApplication.class, args);
+	}
+
+}
---
+++ kbe-ex/src/test/java/com/teamhulo/kbeex/KbeExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhulo.kbeex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
